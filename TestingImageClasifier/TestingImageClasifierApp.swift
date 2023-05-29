2f8e04eb0682682918a9cf2114fb254da9ad9e10
Roba gh <rfalghanmi@gmail.com>
2023-05-29 18:19:36 +0300

    Initial Commit

---
+++ TestingImageClasifier/TestingImageClasifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingImageClasifierApp.swift
+//  TestingImageClasifier
+//
+//  Created by roba on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingImageClasifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
